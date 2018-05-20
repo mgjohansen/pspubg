5f0ba2e834364f59e9a47fedee00ea3b6050707e
Morten G. Johansen <mgj@syspeople.dk>
2018-05-20 18:05:43 +0200

    Fixed typo in code

--- PSPUBG-Players.psm1
+++ PSPUBG-Players.psm1
@@ -27,7 +27,7 @@ function Get-PUBGPlayers {
         'Please make sure the environment variables is set with Set-PUBGApiKey & Set-PUBGRegion before runnning this function'
         break
     }
-    if (($PlayerNames -and $PlayerIDs -eq $false)) {
+    if ($PlayerNames -and $PlayerIDs -eq $false) {
         'You need to either input PlayerNames or PlayerIDs for this to work'
     }
     $RestURL = $Global:PUBGRestUrl + $Global:PUBGRegion
