1d58a5ce34bc1a798b28806e6f7fd772aaa3822c
Morten G. Johansen <mgj@syspeople.dk>
2018-05-29 19:01:07 +0200

    Added Get-PUBGSeasonStats function

--- PSPUBG-Seasons.psm1
+++ PSPUBG-Seasons.psm1
@@ -29,3 +29,34 @@ function Get-PUBGSeasons {
         'There was an error in your request'
     }
 }
+
+<#
+.SYNOPSIS
+    Returns season information for a single player from the connected PUBG Api environment
+    You must have invoked Set-PUBGApiKey and Set-PUBGRegion prior to executing this function
+.EXAMPLE
+    Get-PUBGSeasonStats -PlayerId '659400b28be4471e822e4729586eaa41' -SeasonId 'division.bro.official.2018-05'
+    Returns season information for player id 659400b28be4471e822e4729586eaa41 and the season division.bro.official.2018-05.
+#>
+function Get-PUBGSeasonStats {
+    param(
+        [parameter(mandatory=$true)]
+        [string]$PlayerId,
+        [parameter(mandatory=$true)]
+        [string]$SeasonId
+    )
+    if ((Test-PUBGApiEnvironment) -eq $false) {
+        'Please make sure the environment variables is set with Set-PUBGApiKey & Set-PUBGRegion before runnning this function'
+        break
+    }
+    if ($PlayerId -or $SeasonId -eq $false) {
+        'You need to input PlayerId and SeasonId for this to work'
+    }
+        $RestURL = $Global:PUBGRestUrl + $Global:PUBGRegion + '/players/' + $PlayerId + '/seasons/' + $SeasonId
+        $Result = Invoke-RestMethod -Method Get -Uri $RestURL -ContentType "application/json" -Headers $Global:PUBGApiHeader
+    if ($Result -ne $null) {
+        return $Result
+    } else {
+        'There was an error in your request'
+    }
+}
